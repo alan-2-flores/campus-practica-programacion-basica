187a8f91cf570604864f4cd665ff6923d9145958
Alan Flores <alan.2.flores@atos.net>
2022-07-18 10:21:46 -0500

    Add project files.

---
+++ practicaProgramacionBasica/Fail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace practicaProgramacionBasica
+{
+    public class Fail
+    {
+        public bool comprobacion(string inputFile)
+        {
+            try {
+                using (StreamReader CountriesInput = new StreamReader(inputFile));
+                return true;
+			}
+			catch (FileNotFoundException e){
+				Console.WriteLine(e.Message);
+                return false;
+			}
+        }
+    }
+}
---
+++ practicaProgramacionBasica/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace practicaProgramacionBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fail check = new Fail();
+            DateTime fechaActual = DateTime.Now;
+            string path = @"C:\Users\a875541\Downloads\";
+            string inputFile = path + "Countries.txt";
+            string outputFile = path + "Countries" + fechaActual.ToString("yyyy-MMM-dd")+".txt";
+            if (check.comprobacion(inputFile))
+                GreetCountries(inputFile, outputFile);
+            else {
+                Console.WriteLine("No se pudo leer el archivo");
+                Console.ReadKey();
+            }
+        }
+
+        static void GreetCountries(string inputFile, string outputFile) {
+            string pais;
+            using (StreamReader reader = new StreamReader(inputFile))
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                while ((pais = reader.ReadLine()) != null) //Leemos línea por línea
+                {
+                    writer.WriteLine("Saludos hasta " + pais);
+                }
+                Console.WriteLine("Archivo escrito correctamente");
+                Console.ReadKey();
+            }
+        }
+    }
+}
